b5752d13e82643c99bf265ef2e1bd790fb92d3d4
wd47stpl <wd47.stpl@gmail.com>
2026-07-20 14:47:26 +0530

    working

--- api/mail_config.php
+++ api/mail_config.php
@@ -2,11 +2,11 @@
 
 define('SMTP_HOST', 'mail.infomaniak.com');
 define('SMTP_PORT', 587);
-define('SMTP_USER', 'form@equuschain.io');
-define('SMTP_PASS', 'Q%j1BV+o!4o7%1i4');
+define('SMTP_USER', 'form@thecorporatehub.org');
+define('SMTP_PASS', '933X1YrbuP!&%$Bu');
 define('SMTP_SECURE', 'tls');
 
-define('ADMIN_EMAIL', 'gd34.stpl@gmail.com');
+define('ADMIN_EMAIL', 'form@thecorporatehub.org');
 
-define('SENDER_EMAIL', 'form@equuschain.io');
+define('SENDER_EMAIL', 'form@thecorporatehub.org');
 define('SENDER_NAME', 'The Corporate Hub');
